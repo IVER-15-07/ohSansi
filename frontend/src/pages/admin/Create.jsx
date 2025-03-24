ff696f768c45e12c6783e90ca432b56da64b662a
iver <limber123iver654.12@gmail.com>
2025-03-24 15:10:09 -0400

    definida  la  navegacion del  admin

--- frontend/src/pages/admin/Create.jsx
+++ frontend/src/pages/admin/Create.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Create = () => {
-  return (
-    <div>
-      <h1>hola soy el creador de la olimpiada</h1>
+
+    const [jump, setJump] = useState('CREAR')
+
+    const handleJump = () => {
+        if (jump === 'CREAR') {
+            return <Create />
+        } else if (jump === 'CATEGORIA') {
+            return <Categoria />
+        } else if (jump === 'PAGO') {
+            return <Pago />
+        } else if (jump === 'ABRIR') {
+            return <Abrir />
+        }
+    }
+
+
+
+    return (
+        <div className="flex flex-col h-screen">
+      {/* Sidebar */}
+      <div className="h-1/10 p-4 bg-gray-800 text-white">
+        <h1> crear olimpia                configurar olimpiada  </h1>
+      </div>
+
+      {/* Main Content */}
+      <div className="h-9/10 p-4">
+        <h1>hola aqui esta el contenido de los formularios</h1>
+      </div>
     </div>
-  )
+    )
+
 }
 
 export default Create
---
+++ frontend/src/pages/admin/OlympiadAreas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OlympiadAreas = () => {
+  return (
+    <div>
+        <h1>hola  son  areas de  olimpiada </h1>
+      
+    </div>
+  )
+}
+
+export default OlympiadAreas
